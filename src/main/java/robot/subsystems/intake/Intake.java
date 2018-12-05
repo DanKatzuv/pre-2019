15707f29ca2405ee1bfcc5aeb85cc86cd8ff9331
Unknown <orel.david1502@gmail.com>
2018-12-05 14:28:51 +0200

    intake subsystem base

--- src/main/java/robot/subsystems/intake/Intake.java
+++ src/main/java/robot/subsystems/intake/Intake.java
@@ -1,4 +1,18 @@
 package robot.subsystems.intake;
 
-public class intake {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import robot.Robot;
+import robot.subsystems.drivetrain.commands.JoystickDrive;
+import robot.subsystems.drivetrain.pure_pursuit.Point;
+
+public class Intake extends Subsystem{
+
+
+    @Override
+    public void initDefaultCommand(){
+
+    }
 }
--- src/main/java/robot/subsystems/intake/commands/taking.java
+++ src/main/java/robot/subsystems/intake/commands/taking.java
@@ -1,4 +1,41 @@
 package robot.subsystems.intake.commands;
 
-public class taking {
+import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.subsystems.intake.Intake;
+
+public class taking extends Command {
+    public taking() {
+        requires(Robot.Intake);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {}
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
+}
+
 }
