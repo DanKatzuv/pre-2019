ed5fdee4a2e4339334d3fef5d2f334be17c0fe87
Paulo Khayat <paulokhayat@gmail.com>
2018-12-05 17:52:57 +0200

    Re organize constants

--- src/main/java/robot/subsystems/shooter/Constants.java
+++ src/main/java/robot/subsystems/shooter/Constants.java
@@ -1,10 +1,13 @@
 package robot.subsystems.shooter;
 
 class Constants {
-    static final int kPIDLoopIdx = 0;
-
-    static final int kTimeoutMs = 30; //set to zero to skip waiting for confirmation, set to nonzero to wait and report to DS if action fails.
+    static final boolean TALON_INVERTED = false;
+    static final boolean VICTOR_INVERTED = false;
 
     static final double WHEEL_RADIUS = 0;
+
     static final double kP = 0, kI = 0, kD = 0, kF = 0;
+    static final int kPIDLoopIdx = 0;
+    static final int kTimeoutMs = 30; //set to zero to skip waiting for confirmation, set to nonzero to wait and report to DS if action fails.
+
 }
