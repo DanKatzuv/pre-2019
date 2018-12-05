ac97b0cbd315114a499e52705ae329594902ac82
orel-david <orel.david1502@gmail.com>
2018-12-05 17:31:51 +0200

    Add p constant

--- src/main/java/robot/subsystems/intake/Constants.java
+++ src/main/java/robot/subsystems/intake/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public final static double MIN_PROXIMITY = 0;
     public final static double AXIS_DIAMETER = (1.27) / 100;
     public final static double INTAKE_ARC = (AXIS_DIAMETER * Math.PI);
+    public final static double kP = 0;
 
 
 }
