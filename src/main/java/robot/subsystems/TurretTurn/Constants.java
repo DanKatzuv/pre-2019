6f8c0a5db4001a2be255ba2749f59a85187c24d3
lior <lior5858@gmail.com>
2018-12-05 14:58:35 +0200

    Make constant file for TurretTurn

---
+++ src/main/java/robot/subsystems/TurretTurn/Constants.java
@@ -0,0 +1,8 @@
+package robot.subsystems.TurretTurn;
+
+public class Constants {
+    static final double MAX_VOLTAGE = 4.5;
+    static final double MIN_VOLTAGE = 0.5;
+    static final double MID_VOLTAGE = 2.5;
+    static final double VOLT_DEGREE_PROPORTION = 1/720;
+}
