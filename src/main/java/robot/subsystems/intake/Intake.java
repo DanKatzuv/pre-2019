a634db2d006d5a5e707bc828a035da1627412dc1
Unknown <orel.david1502@gmail.com>
2018-12-05 15:55:15 +0200

    create intake structure

--- src/main/java/robot/subsystems/intake/Intake.java
+++ src/main/java/robot/subsystems/intake/Intake.java
@@ -1,18 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package robot.subsystems.intake;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import robot.Robot;
-import robot.subsystems.drivetrain.commands.JoystickDrive;
-import robot.subsystems.drivetrain.pure_pursuit.Point;
 
-public class Intake extends Subsystem{
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Intake extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    private final VictorSPX LeftIntake = new VictorSPX(Ports.LeftIntake);
+    private final VictorSPX RightIntake = new VictorSPX(Ports.RightIntake);
+    private final VictorSPX Folding = new VictorSPX(Ports.Folding);
 
+
+    public Intake(){
+        LeftIntake.setInverted(false);
+        RightIntake.setInverted(true);
+        Folding.setInverted(false);
+    }
 
     @Override
-    public void initDefaultCommand(){
-
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
     }
 }
---
+++ src/main/java/robot/subsystems/intake/Ports.java
@@ -0,0 +1,7 @@
+package robot.subsystems.intake;
+
+public class Ports {
+    public final static int LeftIntake=0;
+    public  final static int RightIntake=0;
+    public final static int Folding =0;
+}
