e563fb113f01676b3c1977c501026404b8102029
lior <lior5858@gmail.com>
2018-12-05 18:36:21 +0200

    Adjust constants

--- src/main/java/robot/subsystems/TurretTurn/Constants.java
+++ src/main/java/robot/subsystems/TurretTurn/Constants.java
@@ -1,8 +1,9 @@
 package robot.subsystems.TurretTurn;
 
 public class Constants {
-    static final double MAX_VOLTAGE = 4.5;
-    static final double MIN_VOLTAGE = 0.5;
-    static final double MID_VOLTAGE = 2.5;
+    public static final double MAX_DEGREE = 3550;
+    public static final double MIN_DEGREE = 50;
+    static final double MID_DEGREE = 1800;
     static final double VOLT_DEGREE_PROPORTION = 1/720;
+    public static final double SHOOTER_BASE_PERIMITER = 0.2996091;
 }
