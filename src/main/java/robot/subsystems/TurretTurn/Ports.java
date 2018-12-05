bce1bbe912eeb4e0834593801d470c6a481200dd
lior <lior5858@gmail.com>
2018-12-05 18:37:06 +0200

    Adjusted the port names

--- src/main/java/robot/subsystems/TurretTurn/Ports.java
+++ src/main/java/robot/subsystems/TurretTurn/Ports.java
@@ -1,7 +1,6 @@
 package robot.subsystems.TurretTurn;
 
 public class Ports {
-    final static int FrontMotor = 0;
-    final static int BackMotor = 0;
+    final static int Motor = 0;
     final static int Potentiometer = 0;
 }
