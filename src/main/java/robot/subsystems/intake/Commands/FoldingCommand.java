6a37c3d16e99da79e309f7a6036aaf47703d9c63
orel-david <orel.david1502@gmail.com>
2018-12-05 17:37:14 +0200

    Create proportional control on the folding speed for 90 degrees
    and reset and finish condition

--- src/main/java/robot/subsystems/intake/Commands/FoldingCommand.java
+++ src/main/java/robot/subsystems/intake/Commands/FoldingCommand.java
@@ -1,13 +1,16 @@
 package robot.subsystems.intake.Commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.subsystems.intake.Constants;
 
 import static robot.Robot.intake;
 
-/**
+/**@author orel
  *
  */
 public class FoldingCommand extends Command {
+
+    private double speed;
 
     public FoldingCommand() {
         // Use requires() here to declare subsystem dependencies
@@ -17,20 +20,24 @@ public class FoldingCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        intake.reset();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        speed = ((Constants.INTAKE_ARC / 4) - intake.getEncoderDist()) * Constants.kP;
+        intake.setFoldingSpeed(speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
 
-        return false;
+        return intake.intakeIN90DEG();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        intake.endAllMotors();
     }
 
     // Called when another command which requires one or more of the same
