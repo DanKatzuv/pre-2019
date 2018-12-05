27f3bbbd439b56257412e45e01ac6d4bf95fb097
orel-david <orel.david1502@gmail.com>
2018-12-05 16:38:41 +0200

    placeholder for the proximity sensor add to ports

--- src/main/java/robot/subsystems/intake/Ports.java
+++ src/main/java/robot/subsystems/intake/Ports.java
@@ -4,4 +4,5 @@ public class Ports {
     public final static int LeftIntake=0;
     public  final static int RightIntake=0;
     public final static int Folding =0;
+    public final static int proximity = 0;
 }
