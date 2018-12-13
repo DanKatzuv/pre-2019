480aecfdcba27d4231c487de8127c05b779395e1
orel-david <orel.david1502@gmail.com>
2018-12-13 17:17:08 +0200

    Create command group to the shoot with the conveyor turret and shooter

---
+++ src/main/java/robot/subsystems/general_Commands/Complete_shoot.java
@@ -0,0 +1,36 @@
+package robot.subsystems.general_Commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import robot.Robot;
+import robot.subsystems.TurretTurn.commands.TurnByAngle;
+import robot.subsystems.conveyer.Commands.ConveyorCommand;
+import robot.subsystems.shooter.commands.Shoot;
+
+/**
+ *
+ */
+public class Complete_shoot  extends CommandGroup {
+
+    public Complete_shoot (double desired_angle,boolean is_relative, double NTspeed, double timeout) {
+        addSequential(new TurnByAngle(desired_angle,is_relative));//  aim to target
+        addSequential(new ConveyorCommand());//  move ball to turret
+        addSequential(new Shoot(NTspeed,timeout));//then shoot
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+
+    }
+}
